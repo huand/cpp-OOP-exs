2a21698f3e8d3c628c902727166a6d06165d9a8a
Hubert Andre <hubert.andre.1991@gmail.com>
2018-12-16 00:22:01 +0100

    probably last one. good ride and many learnt things for sure :) regretting nothing. i came from a distant place... it;s never too late to start. to give it all.

--- include/Classe_headers/queue.h
+++ include/Classe_headers/queue.h
@@ -1,18 +1,79 @@
-template<class T>
+template <class T>
 class queue
 {
-  private:
-    enum
-    {
-        MAXSIZE = 100
-    };
-    T arr[MAXSIZE];
-    int n;
+private:
+  enum
+  {
+    MAXSIZE = 3
+  };
+  T arr[MAXSIZE];
+  int n;
 
-  public:
-    queue(/* args */);
-    ~queue();
-    void put(T);
-    T get();
-    int inqueue();
+public:
+  queue(/* args */);
+  ~queue();
+  void put(T);
+  T get();
+  int inqueue();
+
+  class QueueFull
+  {
+  };
+
+  class QueueEmpty
+  {
+  };
 };
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+queue<T>::queue(/* args */)
+{
+    n = 0;
+}
+
+template <class T>
+queue<T>::~queue()
+{
+}
+
+template <class T>
+void queue<T>::put(T i)
+{
+
+    if (n < MAXSIZE)
+    {
+        arr[n++] = i;
+    }
+
+    else
+    {
+        throw QueueFull();
+    }
+}
+
+template <class T>
+T queue<T>::get()
+{
+    if (n > 0)
+    {
+        T temp = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            arr[i - 1] = arr[i];
+        }
+        n--;
+        return temp;
+    }
+    else
+    {
+        throw QueueEmpty();
+    }
+}
+template <class T>
+int queue<T>::inqueue()
+{
+    return n;
+}
